b74eb9bd0db50f6eee737675ef6591f88e2af6a2
Simon Siegert <sisiegert@uni-potsdam.de>
2018-05-26 16:02:03 +0200

    Enrich database and extend filter with google.com

--- source/background.js
+++ source/background.js
@@ -1,12 +1,30 @@
 const database = {
   'facebook': 'https://www.facebook.com/',
+  'fb': 'https://www.facebook.com/',
+  'facebook log in': 'https://www.facebook.com/',
   'facebook login': 'https://www.facebook.com/',
+  'facebook.com': 'https://www.facebook.com/',
+  'facebook.de': 'https://www.facebook.de/',
   'stackoverflow': 'https://stackoverflow.com/',
+  'stackoverflow.com': 'https://stackoverflow.com/',
   'netflix': 'https://www.netflix.com/',
+  'netflix.com': 'https://www.netflix.com/',
   'youtube': 'https://www.youtube.com/',
+  'youtube.com': 'https://www.youtube.com/',
+  'youtube.de': 'https://www.youtube.de/',
   'gmx': 'https://www.gmx.net/',
+  'gmx.net': 'https://www.gmx.net/',
+  'gmx.de': 'https://www.gmx.de/',
   'amazon': 'https://www.amazon.com/',
-  'wikipedia': 'https://wikipedia.org/'
+  'amazon.com': 'https://www.amazon.com/',
+  'wikipedia': 'https://wikipedia.org/',
+  'wikipedia.de': 'https://wikipedia.de/',
+  'twitter': 'https://twitter.com/',
+  'twitter.com': 'https://twitter.com/',
+  'ebay': 'https://www.ebay.com/',
+  'ebay.com': 'https://www.ebay.com/',
+  'instagram': 'https://www.instagram.com/',
+  'instagram.com': 'https://www.instagram.com/',
 }
 
 const callback = function (details) {
@@ -24,7 +42,7 @@ const callback = function (details) {
     console.error(error);
   }
 };
-const filter =  {urls: ['*://*.google.de/*']};
+const filter =  {urls: ['*://*.google.de/*', '*://*.google.com/*']};
 const extraInfoSpec = ['blocking'];
 
 chrome.webRequest.onBeforeRequest.addListener(callback, filter, extraInfoSpec);
