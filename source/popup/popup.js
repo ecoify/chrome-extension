8af3e03b3c51580188fc2ff45f868b60322cfe7e
abegehr <anton.begehr@icloud.com>
2018-05-26 20:06:42 +0200

    toggling changes color

--- source/popup/popup.js
+++ source/popup/popup.js
@@ -1,10 +1,20 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function() {
 
   console.log("ecoify popup opened");
 
-  var carbon_grams = document.getElementById('carbon_grams');
+  // toggle
+  var toggle = document.getElementById('toggle');
+  readToggle().then((new_toggle) => {
+    toggle.checked = new_toggle;
+    updateStyleToggle(toggle.checked);
+  });
+  toggle.addEventListener('click', function() {
+    setToggle(toggle.checked);
+    updateStyleToggle(toggle.checked);
+  });
 
   // update carbon grams counter
+  var carbon_grams = document.getElementById('carbon_grams');
   readCounter().then((counter) => {
     console.log("read counter: ", counter);
     new_carbon_grams = counter*0.2;
@@ -21,28 +31,72 @@ window.onload = function () {
     carbon_grams.innerHTML = new_carbon_grams_text;
   });
 
+}, false);
+
+
+function updateStyleToggle(toggle) {
+  var body = document.getElementsByTagName("BODY")[0];
+  if (toggle) {
+    body.classList.remove('off');
+    body.classList.add('on');
+  } else {
+    body.classList.remove('on');
+    body.classList.add('off');
+  }
+}
+
+function readToggle() {
+  return new Promise((resolve, reject) => {
+    getData("ecoify_toggle").then((ecoify_toggle) => {
+      if (typeof ecoify_toggle === 'undefined' || isNaN(ecoify_toggle)) {
+        ecoify_toggle = true;
+      }
+      resolve(ecoify_toggle)
+    })
+  })
+}
+
+function setToggle(new_ecoify_toggle) {
+  console.log("toggled to: ", new_ecoify_toggle);
+  if (new_ecoify_toggle === true || new_ecoify_toggle === false) {
+    setData("ecoify_toggle", new_ecoify_toggle);
+  }
 }
 
 function readCounter() {
   return new Promise((resolve, reject) => {
-    getData("blockedCounter").then((counter) => {
+    getData('blockedCounter').then((counter) => {
       if (typeof counter === 'undefined' || isNaN(counter)) {
         counter = 0;
       }
-      console.log(counter)
       resolve(counter)
     })
   })
 }
 
 function getData(sKey) {
-  return new Promise(function (resolve, reject) {
-    chrome.storage.sync.get(sKey, function (items) {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(sKey, (items) => {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError.message);
         reject(chrome.runtime.lastError.message);
       } else {
         resolve(items[sKey]);
+      }
+    });
+  });
+}
+
+function setData(sKey, sValue) {
+  return new Promise((resolve, reject) => {
+    const data = {}
+    data[sKey] = sValue
+    chrome.storage.sync.set(data, () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        reject(chrome.runtime.lastError.message);
+      } else {
+        resolve(true);
       }
     });
   });
