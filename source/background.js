c1057b62e6478d5e6fc2d5828a81ad5b1a274f67
Simon Siegert <sisiegert@uni-potsdam.de>
2018-05-26 15:28:30 +0200

    Implement main functionality and add example database

--- source/background.js
+++ source/background.js
@@ -1,8 +1,30 @@
-var callback = function(details) {
-  console.log("ecoify: ", details.url);
-};
-var filter =  {urls: ["*://*.google.de/*"]};
-var opt_extraInfoSpec = [];
+const database = {
+  'facebook': 'https://www.facebook.com/',
+  'facebook login': 'https://www.facebook.com/',
+  'stackoverflow': 'https://stackoverflow.com/',
+  'netflix': 'https://www.netflix.com/',
+  'youtube': 'https://www.youtube.com/',
+  'gmx': 'https://www.gmx.net/',
+  'amazon': 'https://www.amazon.com/',
+  'wikipedia': 'https://wikipedia.org/'
+}
 
-chrome.webRequest.onBeforeRequest.addListener(
-  callback, filter, opt_extraInfoSpec);
+const callback = function (details) {
+  try {
+    const match = details.url.match(/&oq=(.+?)&/);
+    if (match) {
+      const newUrl = database[match[1]];
+      if (newUrl) {
+        console.log('ecoify safed 2 g CO2 for: ', match[1]);
+        return {redirectUrl: newUrl};
+      }
+    }
+  }
+  catch (error) {
+    console.error(error);
+  }
+};
+const filter =  {urls: ['*://*.google.de/*']};
+const extraInfoSpec = ['blocking'];
+
+chrome.webRequest.onBeforeRequest.addListener(callback, filter, extraInfoSpec);
